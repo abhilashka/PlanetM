a1c2778955c8b4b1800491ce4a783392f3f0f6f2
abhilash <abhilash.kamble376@gmail.com>
2021-05-10 19:56:22 +0530

    structure created

---
+++ script/script.js
@@ -0,0 +1,36 @@
+$(function () {
+    var $orders = $('#orders');
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:3000/posts',
+        port: 3000,
+        success: function (orders) {
+            $.each(orders, function (i, order) {
+                // $orders.append('<div class="card md-col-2" style="width: 18rem;">  <div class="card-body"> <div class="card-title"> <h4>' + order.title + '</h4><div class="card-text"> ' + order.author + '</div>' + ' </div></div> </div>');
+                $orders.append(
+                    `
+                    <div class= "card" style = "width: 200px; ">
+                    <div class="card-body" style=" background-image: url('${order.images[0].url}');">
+                   
+                    <img src="${order.images[0].url}" alt="img">
+                    <img  class="play" src="https://i.pinimg.com/originals/ef/07/47/ef07471474a0e1086a185086c342ae00.jpg" alt="">
+
+                    </div>`+ `
+
+                    <div class="card-footer">
+                        <a href="#" class="card-link">${order.name}</a>
+                        <p>Album: <a href="#">${order.album_type} </a>
+                        Artist: <a href="#">${order.artists[0].name} </a>
+           
+                        </p>
+                    </div>
+                    </div >
+
+                    `)
+            });
+        }
+    });
+});
+
+
