515ea1d3275ed52e5eb2ba9d0cd4e31aeb8d4670
abhilash <abhilash.kamble376@gmail.com>
2021-05-21 22:39:42 +0530

    header-done

--- script/header.js
+++ script/header.js
@@ -1,4 +1,41 @@
+$(document).ready(function () {
 
+    $('#menu-search').hide()
+
+
+    $('.searchform').hide()
+
+
+
+
+})
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        $('#menu-symbol').show()
+        $('#menu-item').hide()
+
+        $('.logo-text').hide()
+        $('.btn-home-register').hide()
+
+    } else {
+        $('#menu-item').show()
+        $('#menu-symbol').hide()
+        $('.logo-text').show()
+
+
+    }
+}
+
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
+
+
+function showme() {
+    $('#menu-search').show()
+
+}
 
 function explore() {
     location.href = "./ui/discover.html"
@@ -13,8 +50,9 @@ function library() {
     location.href = "./ui/playlist.html"
 }
 
-function login() {
-    location.href = "./ui/login.html"
+function search() {
+    $('.searchform').show()
+
 }
 
 
