03c08daecff430cbf703b92b8f06bfea47275251
abhilash <abhilash.kamble376@gmail.com>
2021-05-21 16:52:41 +0530

    header -update

--- script/container.js
+++ script/container.js
@@ -74,8 +74,8 @@ $(function () {
                 // $songs.append('<div class="card md-col-2" style="width: 18rem;">  <div class="card-body"> <div class="card-title"> <h4>' + song.title + '</h4><div class="card-text"> ' + song.author + '</div>' + ' </div></div> </div>');
                 $songs.append(
                     `
-                    <div class="card">
-                    <div class="card-body"  >
+                    <div class="card1">
+                    <div class="card1-body"  >
                         <div class="image-logo" style="   background-image:url(${song.images[0].url});     background-size: 600px 600px;"
                            >
                             <img src="${song.images[0].url}" alt="">
@@ -85,7 +85,7 @@ $(function () {
         
                     </div>
         
-                    <div class="card-footer">
+                    <div class="card1-footer">
                         <div class="title">${song.name}</div>
                         <div class="artist">${song.album_type} </div>
                         <div class="album">${song.artists[0].name}</div>
