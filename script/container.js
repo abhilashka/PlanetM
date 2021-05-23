9ce669cb7740bbecf7be7153a67283ff54ff3f8a
abhilash <abhilash.kamble376@gmail.com>
2021-05-23 16:51:29 +0530

    container fixed

--- script/container.js
+++ script/container.js
@@ -1,45 +1,31 @@
 
-
 $(function () {
     var $songs = $('#songs');
-
     $.ajax({
         type: 'GET',
         url: 'http://localhost:3000/newrealese',
         port: 3000,
         success: function (songs) {
             $.each(songs, function (i, song) {
-                // $songs.append('<div class="card md-col-2" style="width: 18rem;">  <div class="card-body"> <div class="card-title"> <h4>' + song.title + '</h4><div class="card-text"> ' + song.author + '</div>' + ' </div></div> </div>');
                 $songs.append(
-                    `
-                    <div class="card1 resource-box">
-                    <div class="card1-body"  >
-                        <div class="image-logo" style="   background-image:url(${song.images[0].url});     background-size: 600px 600px;"
-                           >
+                    ` <div class="card1 resource-box">
+                    <div class="card1-body">
+                        <div class="image-logo" style=" background-image:url(${song.images[0].url}); background-size: 600px 600px;">
                             <img src="${song.images[0].url}" alt="">
-        
                         </div>
-        
-        
                     </div>
-        
                     <div class="card1-footer">
                         <div class="title">${song.name}</div>
                         <div class="artist">${song.album_type} </div>
                         <div class="album">${song.artists[0].name}</div>
-        
                     </div>
-                </div>
-        
-
-                    `)
+                </div>`
+                )
             });
         }
     });
 
 });
-
-
 
 
 
@@ -55,29 +41,19 @@ $(function () {
         port: 3000,
         success: function (songs) {
             $.each(songs, function (i, song) {
-                // $songs.append('<div class="card md-col-2" style="width: 18rem;">  <div class="card-body"> <div class="card-title"> <h4>' + song.title + '</h4><div class="card-text"> ' + song.author + '</div>' + ' </div></div> </div>');
-
                 if (song.language === "hindi") {
                     $hindi.append(
-                        `
-                    <div class="card1 resource-box" id="${song.url}" onclick="playsong(this)">
-                    <div class="card1-body"  >
-                        <div class="image-logo" style="   background-image:url(${song.img});     background-size: 600px 600px;"
-                           >
-                            <img src="${song.img}" alt="">
-        
+                        `<div class="card1 resource-box" id="${song.url}" onclick="playsong(this)">
+                        <div class="card1-body">
+                            <div class="image-logo" style="   background-image:url(${song.img});  background-size: 600px 600px;">
+                                <img src="${song.img}" alt="">
+                            </div>
                         </div>
-        
-        
-                    </div>
-        
-                    <div class="card1-footer">
-                        <div class="title">${song.name}</div>
-                        <div class="album">${song.artist}</div>
-        
-                    </div>
-                </div>
-        
+                        <div class="card1-footer">
+                            <div class="title">${song.name}</div>
+                            <div class="album">${song.artist}</div>
+                        </div>
+                        </div>
                     `)
 
                 }
@@ -85,77 +61,53 @@ $(function () {
 
                 if (song.language === "english") {
                     $english.append(
-                        `
-                    <div class="card1 resource-box" id="${song.url}" onclick="playsong(this)">
-                    <div class="card1-body"  >
-                        <div class="image-logo" style="   background-image:url(${song.img});     background-size: 600px 600px;"
-                           >
-                            <img src="${song.img}" alt="">
-        
+                        `<div class="card1 resource-box" id="${song.url}" onclick="playsong(this)">
+                        <div class="card1-body">
+                            <div class="image-logo" style="   background-image:url(${song.img});  background-size: 600px 600px;">
+                                <img src="${song.img}" alt="">
+                            </div>
                         </div>
-        
-        
-                    </div>
-        
-                    <div class="card1-footer">
-                        <div class="title">${song.name}</div>
-                        <div class="album">${song.artist}</div>
-        
-                    </div>
-                </div>
-        
+                        <div class="card1-footer">
+                            <div class="title">${song.name}</div>
+                            <div class="album">${song.artist}</div>
+                        </div>
+                        </div>
                     `)
 
                 }
 
                 if (song.language === "marathi") {
                     $marathi.append(
-                        `
-                        <div class="card1 resource-box" id="${song.url}" onclick="playsong(this)">
-                        <div class="card1-body"  >
-                            <div class="image-logo" style="   background-image:url(${song.img});     background-size: 600px 600px;"
-                               >
+                        `<div class="card1 resource-box" id="${song.url}" onclick="playsong(this)">
+                        <div class="card1-body">
+                            <div class="image-logo" style="   background-image:url(${song.img});  background-size: 600px 600px;">
                                 <img src="${song.img}" alt="">
-            
                             </div>
-            
-            
                         </div>
-            
                         <div class="card1-footer">
                             <div class="title">${song.name}</div>
                             <div class="album">${song.artist}</div>
-            
                         </div>
-                    </div>
-            
-                        `)
+                        </div>
+                    `)
 
                 }
 
 
                 if (song.language === "spanish") {
                     $spanish.append(
-                        `
-                        <div class="card1 resource-box" id="${song.url}" onclick="playsong(this)">
-                        <div class="card1-body"  >
-                            <div class="image-logo" style="   background-image:url(${song.img});     background-size: 600px 600px;"
-                               >
+                        `<div class="card1 resource-box" id="${song.url}" onclick="playsong(this)">
+                        <div class="card1-body">
+                            <div class="image-logo" style="   background-image:url(${song.img});  background-size: 600px 600px;">
                                 <img src="${song.img}" alt="">
-            
                             </div>
-            
-            
                         </div>
-            
                         <div class="card1-footer">
                             <div class="title">${song.name}</div>
                             <div class="album">${song.artist}</div>
-            
                         </div>
-                    </div>
-            
-                        `)
+                        </div>
+                    `)
 
                 }
             });
