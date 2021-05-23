288277ab96c46bf929a3623f218854126d58171a
abhilash <abhilash.kamble376@gmail.com>
2021-05-23 16:57:14 +0530

    js fixed

--- script/playground.js
+++ script/playground.js
@@ -1,22 +1,16 @@
 $(document).ready(function () {
-    // click on button submit
     $("#submit").on('click', function () {
-        // send ajax
         let playlistz={
             title:$("#title").val(),
             description:$("#description").val(),
             user_id:sessionStorage.getItem("id")
         }
         $.ajax({
-            url: 'http://localhost:3000/playlist/', // url where to submit the request
-            type: "POST", // type of action POST || GET
-            dataType: 'json', // data type
-            // data: $("#form").serialize()+sessionStorage.getItem("name"), // post data || get data
+            url: 'http://localhost:3000/playlist/', 
+            type: "POST", 
+            dataType: 'json', 
             data:playlistz,
             success: function (result) {
-                // you can see the result from the console
-                // tab of the developer tools
-                // console.log(result);
                 console.log(playlistz);
             },
             error: function (xhr, resp, text) {
@@ -31,7 +25,6 @@ $(document).ready(function () {
 $(function () {
     var $playlist = $('#playlist');
     var current_user=sessionStorage.getItem("id");
-
     $.ajax({
         type: 'GET',
         url: 'http://localhost:3000/playlist',
@@ -41,7 +34,6 @@ $(function () {
             $.each(playlist, function (i, song) {
                 if(sessionStorage.getItem("id")!=null){
                     if(song.user_id==current_user){
-                // $songs.append('<div class="card md-col-2" style="width: 18rem;">  <div class="card-body"> <div class="card-title"> <h4>' + song.title + '</h4><div class="card-text"> ' + song.author + '</div>' + ' </div></div> </div>');
                 $playlist.append(
                     `
                    <div class="playlist-item">${song.title}</div>
