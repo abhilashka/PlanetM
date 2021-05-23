f1837f01bee380b5adba4f4e3822e6e5ae5132da
abhilash <abhilash.kamble376@gmail.com>
2021-05-23 23:46:19 +0530

    player

--- script/player.js
+++ script/player.js
@@ -1,38 +1,43 @@
 
-
+let isPlaying = false;
+var curr_track;
 function playsong(d) {
 
     console.log(d.getAttribute("id"))
 
 
-    var audioElement = new Audio(d.getAttribute("id"))
+
+    playpauseTrack(d.getAttribute("id"));
 
 
 
-
-    d.setAttribute("track", "playing");
-
+}
 
 
-    if ($('div').hasClass('play')) {
-        $('div').removeClass('play');
-        audioElement.pause();
+function playpauseTrack(d) {
+    // Switch between playing and pausing
+    // depending on the current state
+    if (!isPlaying) playTrack(d);
+    else pauseTrack();
+}
 
-    } else {
-        $('div').addClass('play');
-        audioElement.play();
+function playTrack(d) {
+    // Play the loaded track
 
-    }
+    curr_track = new Audio(d)
+    curr_track.play();
+    isPlaying = true;
 
+    // Replace icon with the pause icon
+    // playpause_btn.innerHTML = '<i class="fa fa-pause-circle fa-5x"></i>';
+}
 
+function pauseTrack(d) {
+    // Pause the loaded track
+    curr_track.pause();
+    isPlaying = false;
 
-
-    $('#play-pause').click(function () {
-
-        audioElement.pause()
-
-
-    })
-
+    // Replace icon with the play icon
+    // playpause_btn.innerHTML = '<i class="fa fa-play-circle fa-5x"></i>';;
 }
 
