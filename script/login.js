81de2c865cc6065374ce36ee702cc28b3f0d41a5
cyrilsibi99 <83388865+cyrilsibi99@users.noreply.github.com>
2021-05-24 00:02:36 +0530

    Email/Phone already exists added

--- script/login.js
+++ script/login.js
@@ -1,219 +1,250 @@
 $(function () {
-    let error_user = false;
-    let error_password = false;
-    let error_fname = false;
-    let error_email = false;
-    let error_number = false
-    let error_passw = false;
-    let error_retype_password = false;
+  let error_user = false;
+  let error_password = false;
+  let error_fname = false;
+  let error_email = false;
+  let error_number = false;
+  let error_passw = false;
+  let error_retype_password = false;
 
+  $(".error-msg").hide();
 
-    $(".error-msg").hide()
-
-    //username validation starts
-    function check_user() {
-        let value = $('#username').val()
-        let expression = /^[6-9]\d{9}|([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/
-        if (value = '' || !value.match(expression)) {
-            $('#error-user').show()
-            error_user = true
-        } else {
-            $('#error-user').hide()
-
-        }
+//   Validate name
+  function check_fname() {
+    let pattern = /^[a-zA-Z\s]*$/;
+    let fname = $("#name").val();
+    if (pattern.test(fname) && fname !== "") {
+      $("#error-name").hide();
+    } else {
+      $("#error-name").html("Should contain only Characters");
+      $("#error-name").show();
+      error_fname = true;
     }
-    //username validation ends
+  }
 
-    function check_password() {
-        let password_length = $("#pass").val().length;
-        if (password_length < 8) {
-            $("#error-password").html("Atleast 8 Characters");
-            $("#error-password").show();
-            $("#password").css("border-bottom", "2px solid #F90A0A");
-            error_password = true;
-        } else {
-            $("#error-password").hide();
-        }
+ //Validate password for login page
+  function check_password() {
+    let password_length = $("#pass").val().length;
+    if (password_length < 8) {
+      $("#error-password").html("Atleast 8 Characters");
+      $("#error-password").show();
+      $("#password").css("border-bottom", "2px solid #F90A0A");
+      error_password = true;
+    } else {
+      $("#error-password").hide();
     }
+  }
 
-
-    function check_fname() {
-        let pattern = /^[a-zA-Z\s]*$/;
-        let fname = $("#name").val();
-        if (pattern.test(fname) && fname !== "") {
-            $("#error-name").hide();
-
-        } else {
-            $("#error-name").html("Should contain only Characters");
-            $("#error-name").show();
-            error_fname = true;
-        }
+//username validation  for login page
+  function check_user() {
+    let value = $("#username").val();
+    let expression =
+      /^[6-9]\d{9}|([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    if ((value = "" || !value.match(expression))) {
+      $("#error-user").show();
+      error_user = true;
+    } else {
+      $("#error-user").hide();
     }
+  }
 
-    function check_email() {
-        let pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
-        let email = $("#email").val();
-        if (pattern.test(email) && email !== "") {
-            $("#error-email").hide();
-        } else {
-            $("#error-email").show();
-            error_email = true;
-        }
+//   Validate email id
+  function check_email() {
+    let pattern = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;
+    let email = $("#email").val();
+    if (pattern.test(email) && email !== "") {
+      $("#error-email").hide();
+    } else {
+      $("#error-email").show();
+      error_email = true;
     }
+  }
 
-    //mobile number 
-    function check_phone() {
-        let $regexNumber = /^[6-9]\d{9}$/
-        let numberValue = $("#phone").val()
-        if (numberValue < 999999999 || !numberValue.match($regexNumber)) {
-            $("#error-phone").show()
-            error_number = true
-            return true
-        } else {
-            $("#error-phone").hide()
-        }
+  //validate mobile number
+  function check_phone() {
+    let $regexNumber = /^[6-9]\d{9}$/;
+    let numberValue = $("#phone").val();
+    if (numberValue < 999999999 || !numberValue.match($regexNumber)) {
+      $("#error-phone").show();
+      error_number = true;
+      return true;
+    } else {
+      $("#error-phone").hide();
     }
+  }
 
-    //Check password in register form
-    function check_passw() {
-        let password_length = $("#passw").val().length;
-        if (password_length < 8) {
-            $("#error-reg-pass").show();
-            error_passw = true;
-        } else {
-            $("#error-reg-pass").hide();
-        }
+  //Check password in register form
+  function check_passw() {
+    let password_length = $("#passw").val().length;
+    if (password_length < 8) {
+      $("#error-reg-pass").show();
+      error_passw = true;
+    } else {
+      $("#error-reg-pass").hide();
     }
+  }
 
-    //Confirm password in register form
-    function check_retype_password() {
-        let password = $("#passw").val();
-        let retype_password = $("#repass").val();
-        if (password !== retype_password) {
-            $("#error-reg-confpass").show();
-            error_retype_password = true;
-        } else {
-            $("#error-reg-confpass").hide();
-
-        }
+  //Confirm password in register form
+  function check_retype_password() {
+    let password = $("#passw").val();
+    let retype_password = $("#repass").val();
+    if (password !== retype_password) {
+      $("#error-reg-confpass").show();
+      error_retype_password = true;
+    } else {
+      $("#error-reg-confpass").hide();
     }
+  }
+  
 
-    $("#username").keyup(function () {
-        check_user();
-    });
-    $("#pass").keyup(function () {
-        check_password();
-    });
-    $("#name").keyup(function () {
-        check_fname();
-    });
-    $("#email").keyup(function () {
-        check_email();
-    });
-    $("#phone").keyup(function () {
-        check_phone()
-    });
-    $("#passw").keyup(function () {
-        check_passw();
-    });
-    $("#repass").keyup(function () {
-        check_retype_password();
-    });
+//   performing validations on keyup
 
-
-
-
-    $("#login_form").submit(function (e) {
-        e.preventDefault()
-
-        error_user = false;
-        error_password = false;
-
-        check_user();
-        check_password();
-
-        if (error_user === false && error_password === false) {
-
-            let user = $("#username").val()
-            let password = $("#pass").val()
-            exprss = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/
-            const myUrl = new URL('http://localhost:3000/users/')
-            if ($("#username").val().match(exprss)) {
-                myUrl.searchParams.set("email", user)
-            } else {
-                myUrl.searchParams.set("mobile", user)
-            }
-            myUrl.searchParams.set("password", btoa(password))
-            myUrl.searchParams.get("name")
+  $("#username").keyup(function () {
+    check_user();
+  });
+  $("#pass").keyup(function () {
+    check_password();
+  });
+  $("#name").keyup(function () {
+    check_fname();
+  });
+  $("#email").keyup(function () {
+    check_email();
+  });
+  $("#phone").keyup(function () {
+    check_phone();
+  });
+  $("#passw").keyup(function () {
+    check_passw();
+  });
+  $("#repass").keyup(function () {
+    check_retype_password();
+  });
 
 
-            console.log(myUrl)
+// Login Form starts here
 
+  $("#login_form").submit(function (e) {
+    e.preventDefault();
+
+    error_user = false;
+    error_password = false;
+
+    check_user();
+    check_password();
+
+    if (error_user === false && error_password === false) {
+      let user = $("#username").val();
+      let password = $("#pass").val();
+      exprss = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/;           //check for email
+      const myUrl = new URL("http://localhost:3000/users/");
+      if ($("#username").val().match(exprss)) {                 //search parameters 
+        myUrl.searchParams.set("email", user);                  
+      } else {
+        myUrl.searchParams.set("mobile", user);
+      }
+      myUrl.searchParams.set("password", btoa(password));
+      myUrl.searchParams.get("name");
+
+      $.ajax({
+        type: "GET",
+        url: myUrl,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (result) {
+          if (result.length != 0) {
+            var id = result[0].id;
+            let name1 = result[0].name;
+            sessionStorage.setItem("name", name1);
+            sessionStorage.setItem("id", id);
+            location.href = "./ui/home.html";
+          } else {
+            $("#error-login").show();
+          }
+        },
+      });
+    } else {
+      $("#error-login").show();
+    }
+  });
+
+
+  // Registration Form starts here 
+  $("#registration_form").submit(function (e) {
+    e.preventDefault();
+
+    error_fname = false;
+    error_email = false;
+    error_number = false;
+    error_passw = false;
+    error_retype_password = false;
+
+    // Validating all parameters individually
+    check_fname();
+    check_email();
+    check_phone();                  
+    check_passw();
+    check_retype_password();
+
+    if (
+      error_fname === false &&
+      error_email === false &&
+      error_number === false &&
+      error_passw === false &&
+      error_retype_password === false
+    ) {
+      var email = $("#email").val();
+      var mobile = $("#phone").val();
+
+      let users = {
+        name: $("#name").val(),
+        email: $("#email").val(),
+        mobile: $("#phone").val(),
+        password: btoa($("#passw").val()),
+      };
+      $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "http://localhost:3000/users?email=" + email,
+        async: false,
+        success: function (data) {
+          console.log(data);
+
+//Check if email already exists
+          if (data[0] == undefined) {
+            $("#error-email-reg").hide();
+            $("#error-phone-reg").hide();
             $.ajax({
-                type: "GET",
-                url: myUrl,
-                contentType: "application/json; charset=utf-8",
-                dataType: 'json',
-                success: function (result) {
-                    if (result.length != 0) {
-                        var id = result[0].id;
-                        let name1 = result[0].name;
-                        sessionStorage.setItem('name', name1);
-                        sessionStorage.setItem('id', id);
-                        console.log("Logged in as " + name1);
-                        location.href = "./ui/home.html";
-                        console.log("Hello");
-                    } else {
-                        $('#error-login').show();
-                    }
+              type: "GET",
+              dataType: "json",
+              url: "http://localhost:3000/users?mobile=" + mobile,
+              async: false,
+              success: function (data) {
+//Check if mobile number already exists
+                if (data[0] == undefined) {
+                  $.ajax({
+                    type: "POST",
+                    dataType: "json",
+                    url: "http://localhost:3000/users",
+                    data: users,
+                    async: false,
+                    success: function (data) {
+                      console.log(data + "added");
+                    },
+                  });
+                } else {
+                  $("#error-phone-reg").show();
                 }
+              },
             });
-
-        } else {
-            $('#error-login').show();
-        }
-    })
-
-
-
-    $("#registration_form").submit(function (e) {
-        e.preventDefault()
-
-        error_fname = false;
-        error_email = false;
-        error_number = false
-        error_passw = false;
-        error_retype_password = false;
-
-        check_fname();
-        check_email();
-        check_phone();
-        check_passw();
-        check_retype_password();
-
-        if (error_fname === false && error_email === false && error_number === false && error_passw === false && error_retype_password === false) {
-            let users = {
-                name: $("#name").val(),
-                email: $("#email").val(),
-                mobile: $("#phone").val(),
-                password: btoa($("#passw").val())
-            }
-
-            $.ajax({
-                type: "POST",
-                dataType: "json",
-                url: "http://localhost:3000/users",
-                data: users,
-                success: function (data) {
-                    console.log(data)
-                }
-            })
-        } else {
-            $('#error-reg').show();
-            return false;
-        }
-
-    })
-
-})
-
+          } else {
+            $("#error-email-reg").show();
+          }
+        },
+      });
+    } else {
+      $("#error-reg").show();
+      return false;
+    }
+  });
+});
