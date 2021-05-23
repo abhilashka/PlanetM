ef7ffc58ca1d09098f00b956c26965661b157177
akashta <62641503+Devil983@users.noreply.github.com>
2021-05-23 17:28:01 +0530

    update done

--- script/script.js
+++ script/script.js
@@ -1,6 +1,3 @@
-
-
-
 // ---------------   This API is for loading music   --------------------//
 
 let ajax1 = $.ajax({
@@ -9,9 +6,9 @@ let ajax1 = $.ajax({
   dataType: "json",
   async: true,
   success: function (data) {
-    console.log("From the first requests");
-    let products = "";
-    $.each(data, function (i, v) {
+  console.log("From the first requests");
+  let products = "";
+  $.each(data, function (i, v) {
       products += `
                 <div class="resource-box" >
                 <div id=${v.url}>
@@ -27,41 +24,33 @@ let ajax1 = $.ajax({
             `
 
     })
-
-    $(".inside-main-content").append(products)
-
+  $(".inside-main-content").append(products)
   },
   error: function () {
     console.log("not able to process request");
   }
-
 })
 
 
-//This code is for search the music in the searcharea
+//This is for searching  the music in the searcharea
 
 $("#searchmusic").on("keyup", function () {
   let key = $(this).val().toLowerCase();
   console.log(key)
-
   $(".resource-box").filter(function () {
-    $(this).toggle($(this).text().toLowerCase().indexOf(key) > -1);
+  $(this).toggle($(this).text().toLowerCase().indexOf(key) > -1);
   });
 });
 
-// After clicked on song
 
+// After clicked on song
 $("body").on('click', '.resource-box', function () {
   let songurl = $(this).attr('id');
   // alert("Played : " + songurl);
 });
-
-// });   
+ 
 
 //After clicked on search button
-
-
-
 //  $(document).ready(function() {
 //   console.log("hello")
 // Search
@@ -69,7 +58,6 @@ $("body").on('click', '.resource-box', function () {
 // // $('.xyz').click(function() {
 //   $("#searchmusic").show();
 //   console.log("hello")
-
 // }
 // if()
 // {
@@ -85,36 +73,21 @@ $("body").on('click', '.resource-box', function () {
 
 
 $(document).ready(function () {
-
-  $(".xyz").click(function () {
-    console.log(`clikck me `)
+    $(".xyz").click(function () {
+    console.log(`click me `)
     $("#searchmusic").toggle();
   });
 });
 
 $(".dropdown-menu a").on("click", function () {
   var ak = $(this).attr("id");
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
+
 //this API is for fetching the english songs
 
 $(".dropdown-menu a").on("click", function () {
-
   var a_href = $(this).attr("id");
   console.log(a_href);
-
   $.ajax({
     type: "GET",
     url: "http://localhost:3000/music?language=" + a_href,
@@ -124,35 +97,23 @@ $(".dropdown-menu a").on("click", function () {
       console.log(data);
       let audios = "";
       $.each(data, function (i, v) {
-
-
-        audios += `
-          
+       audios += `   
         <div id="${v.url}" value="${v.language}" class="resource-box">
         <div class="song-image">
         <img class="img-fluid"  src=${v.img} />
         </div>
         <div class="song-title">
           <strong><span class="title">${v.name}</span></strong>
- 
+        </div>
          </div>
-         </div>
-
-
-           `;
+         `;
       });
-
       $(".inside-main-content").empty();
       $(".inside-main-content").append(audios);
-
     },
-
-
     error: function () {
       console.log("songs are not available");
     },
-
-
   });
 });
 
@@ -160,5 +121,3 @@ $(".dropdown-menu a").on("click", function () {
 
 
 
-
-/* */
